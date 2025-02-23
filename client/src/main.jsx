4f12db2df41525db5788e5c6ca753c8065872435
Tanishk Dhope <tanishkdhope@gmail.com>
2025-02-23 14:42:50 +0530

    pwa functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { SocketProvider } from "./contexts/socketContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")).render(
   <ThemeProvider>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-})
+  plugins: [
+    tailwindcss(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "TetherChat",
+        short_name: "TChat",
+        description: "Cool Chat App",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+      },
+    }),
+  ]
+}); 
+
